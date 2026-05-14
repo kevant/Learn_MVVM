cb01451394847f6562c3d2b78a6111bf66716f22
kenyang <kenyang.tan@gmail.com>
2026-05-14 09:12:15 +0800

    Add application mutex project

---
+++ app_mutex/App.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
+
+namespace app_mutex
+{
+    public partial class App : Application
+    {
+        private static Mutex _mutex;
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            const string mutexName = "SingleInstanceWpfApp";
+
+            // for machine-wide single instance
+            //const string mutexName = @"Global\CompanyName_MyWpfApp";
+
+            bool createdNew;
+            _mutex = new Mutex(true, mutexName, out createdNew);
+
+            if (!createdNew)
+            {
+                BringExistingInstanceToFront();
+                Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
+        }
+
+        private void BringExistingInstanceToFront()
+        {
+            Process current = Process.GetCurrentProcess();
+
+            foreach (Process process in Process.GetProcessesByName(current.ProcessName))
+            {
+                if (process.Id != current.Id)
+                {
+                    IntPtr handle = process.MainWindowHandle;
+
+                    if (handle != IntPtr.Zero)
+                    {
+                        SetForegroundWindow(handle);
+                    }
+
+                    break;
+                }
+            }
+        }
+    }
+
+}
